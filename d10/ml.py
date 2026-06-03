2fc429217924a5bfbbf2f7bdb27c1c559bff54b6
kanetshubhangi169-sys <kanetshubhangi169@gmail.com>
2026-06-03 10:59:07 +0530

    Added new files

--- d10/ml.py
+++ d10/ml.py
@@ -47,27 +47,6 @@ else:
 print("score:",score)
 
 
-#linear regression
-from sklearn.linear_model import LinearRegression
-import numpy as np
 
-x = np.array([1,2,3,5,6]).reshape(-1,1)
-y = np.array([10,20,30,50,60])
 
-model = LinearRegression()
-model.fit(x,y)
-prediction = model.predict([[4]])
-print(prediction[0])
 
-#logistic regression
-from sklearn.linear_model import LogisticRegression
-import numpy as np
-
-x = np.array([1,2,3,4,5,6,8,9,]).reshape(-1,1)
-y = np.array([0,0,0,1,1,1,1,1])
-
-model = LogisticRegression()
-model.fit(x,y)
-
-prediction = model.predict([[7]])
-print("predicted:",prediction[0])
